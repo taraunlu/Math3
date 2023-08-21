f479349e2baa8f13bf673ed8adb3268925d14df0
taraunlu <tarau@metropolia.fi>
2023-08-21 12:27:14 +0300

    Done

--- 1/assignment2.py
+++ 1/assignment2.py
@@ -7,7 +7,7 @@ y = 4.7
 hypotenuusa = math.hypot(x, y)
 
 kulma1 = math.degrees(math.asin(y / hypotenuusa))
-kulma2 = 180-90-63.9
+kulma2 = 180 - 90 - 63.9
 
 print(f"Hypotenuusan pituus: {hypotenuusa:.1f}")
 print(f"Kaksi muuta kulmaa: {kulma1:.1f}° ja {kulma2:.1f}°")
